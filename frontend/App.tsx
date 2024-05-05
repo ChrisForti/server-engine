30a6af61ed313138b8771658e4e9783183f13b9c
Christopher Forti <christopher.forti.79@gmail.com>
2024-05-04 20:21:19 -0400

    Making to do list relevant.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -7,10 +7,20 @@ function App() {
       <h1>To Do List</h1>
       <div>
         <li>
-          <ToDoListItem isComplete={false}>Item 1</ToDoListItem>;
+          <ToDoListItem isComplete={false}>
+            Item 1: Set up personal hosting enviroment, via RPI technology.
+            <ul>docker</ul>
+            <ul>DNS</ul>
+          </ToDoListItem>
+          ;
         </li>
         <li>
-          <ToDoListItem isComplete={false}>Item 2</ToDoListItem>;
+          <ToDoListItem isComplete={false}>
+            Item 2: Finish RPI time-lapse retrofit.<ul>Housing upgrade</ul>
+            <ul>New camera</ul>
+            <ul>You tube of the build</ul>
+          </ToDoListItem>
+          ;
         </li>
         <li>
           <ToDoListItem isComplete={false}>Item 3</ToDoListItem>;
